af97cadb4e8373fd07168dfab14378b5f9a99333
Gazeshooter <221618862+Gazeshooter@users.noreply.github.com>
2026-08-05 04:56:30 +0100

    Add Dreamway activation cast and refine portal placement

---
+++ data/sql/db-world/base/0003_emerald_dreamway_spell_script.sql
@@ -0,0 +1,6 @@
+DELETE FROM `spell_script_names`
+WHERE `spell_id` = 60957
+  AND `ScriptName` = 'spell_emerald_dreamway_activation';
+
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`)
+VALUES (60957, 'spell_emerald_dreamway_activation');
